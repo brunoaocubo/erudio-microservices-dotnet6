37c77be27d5ae8adbdd7221bf00e1dda1197f181
Bruno Tiago <116372081+brunoaocubo@users.noreply.github.com>
2023-07-14 10:48:14 -0300

    FirstMicrosservices

---
+++ S07_ErudioMicroservices.NET6-FirstMicrosservices/GeekShopping/GeekShopping.ProductAPI/Model/Base/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GeekShopping.ProductAPI.Model.Base
+{
+    public class BaseEntity
+    {
+        [Key]
+        [Column("id")]
+        public long Id { get; set; }
+    }
+}
